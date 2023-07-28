70b9a62579b083f92c24f795fb39a82ed8205847
Lubnnia <lubnnia@gmail.com>
2023-07-28 10:20:14 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { app } from './app'
+import { knex } from './database'
+import { env } from './env'
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
+
+app.listen(
+  {
+    host: '0.0.0.0',
+    port: env.PORT,
+  },
+  () => {
+    console.log('🚀 Server is listening')
+  },
+)
