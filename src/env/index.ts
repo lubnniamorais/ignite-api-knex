d6474f62943b63eb00500632e935ec7330e49a8e
Lubnnia <lubnnia@gmail.com>
2023-08-06 20:46:54 -0300

    refactor: Update files in project

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
 });
 
